870b543322f44caa3266b65dab1db413ae7b7733
Joseph Quiroz <me@josephquiroz.com>
2024-05-09 12:07:08 -0500

    inicio del proyecto

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:authentication_micro_app/sign_in_view.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SignInView(),
+    );
+  }
+}
